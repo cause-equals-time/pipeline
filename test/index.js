8fb525a9ea7bc3f2fd7a41d8d7ab72f04c152c4b
cause.equals.time <joaodgdias@gmail.com>
2022-03-25 12:27:59 +0100

    7

--- test/index.js
+++ test/index.js
@@ -3,31 +3,14 @@ let expect = require("chai").expect;
 const axios = require('axios');
 axios.defaults.baseURL = 'https://4qofk8772j.execute-api.eu-central-1.amazonaws.com/Prod';
 
-/* async function dostuff(){
-    const input = {name:"daniel", age:17};
-    try{
-    const item = (await axios.put(`/resource/${Date.now()}`, input));
-    console.log(item);
-    }
-    catch (e){
-        console.log(e);
-    }
-}
-dostuff(); */
 
-async function run(){
-    const input = {name:"daniel", age:17};
-    const item = (await axios.put(`/resource/${Date.now()}`, input)).data;
-    console.log(item);
-}
-run();
 describe("CRUD API testing", () => {
     const input = {id:Date.now(), name:"daniel", age:17};
     describe("PUT item", function() {
         it("returns the item", async function() {
             const response = await axios.put(`/resource/${input.id}`, input);
             expect(response.status).to.equal(200);
-            expect(response.data.id).to.equal(input.id);
+            expect(response.data.id).to.equal(input.id.toString());
             expect(response.data.name).to.equal("daniel");
             expect(response.data.age).to.equal(17);
         });      
@@ -37,7 +20,7 @@ describe("CRUD API testing", () => {
         it("returns the item", async function() {
             const response = await axios.get(`/resource/${input.id}`);
             expect(response.status).to.equal(200);
-            expect(response.data.id).to.equal(input.id);
+            expect(response.data.id).to.equal(input.id.toString());
             expect(response.data.name).to.equal("daniel");
             expect(response.data.age).to.equal(17);
         });  
